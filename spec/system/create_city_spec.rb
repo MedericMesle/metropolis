2575353f8118cfdaefb050636bc02da0be9f1a5d
mederic mesle <mederic.mesle@gmail.com>
2022-05-04 09:47:43 +0200

    First test passing

--- spec/system/create_city_spec.rb
+++ spec/system/create_city_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'Creating a city', type: :system do
   scenario 'valid inputs' do
     visit new_city_path
     fill_in 'Name', with: 'Chicago'
-    click_on "Create city"
+    click_on "Create City"
     visit cities_path
     expect(page).to have_content('Chicago')
   end
